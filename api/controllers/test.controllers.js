90053c1aad2b8b75030f9299fafe172cfb75b3db
Po Bidau <pobidau@protonmail.com>
2026-04-13 20:03:38 +0200

    feat: création du middlware pour vérifier le token

--- api/controllers/test.controllers.js
+++ api/controllers/test.controllers.js
@@ -1,16 +1,9 @@
 import jwt from 'jsonwebtoken';
 
 export const shbeLoggedIn = (req, res) => {
-  // 1. Check the token (pour pouvoir être authentifier)
-  const token = req.cookies.token;
-
-  // si pas de tocken, pas d'acces
-  if (!token) return res.status(401).json({ message: 'Not Authenticated!' });
-
-  // Si possède un token, vérification validité (durée de validité, id)
-  jwt.verify(token, process.env.JWT_SECRET_KEY, async (err, payload) => {
-    if (err) return res.status(403).json({ message: 'Token is not Valid' });
-  });
+  // 1.Verification token faite pas le middlware
+  // 2. Récupération de l'id (utilise)
+  console.log(req.userId);
 
   // Si tout est ok
   res.status(200).json({ message: 'You are authentificated' });
--- api/routes/test.routes.js
+++ api/routes/test.routes.js
@@ -1,9 +1,10 @@
 import express from 'express';
 import { shbeAdmin, shbeLoggedIn } from '../controllers/test.controllers.js';
+import { verifyToken } from '../middleware/verifyToken.js';
 
 const testRouter = express.Router();
 
-testRouter.get('/shbe-logged-in', shbeLoggedIn);
+testRouter.get('/shbe-logged-in', verifyToken, shbeLoggedIn);
 
 testRouter.get('/shbe-admin', shbeAdmin);
 
