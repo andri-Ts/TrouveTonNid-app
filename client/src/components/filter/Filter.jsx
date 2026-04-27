4492e1dbeb15dc9cea2b0485b653a120d9e90dfa
Po Bidau <pobidau@protonmail.com>
2026-04-27 11:51:25 +0200

    feat: implémentation du filtre dans page recherche

--- client/src/components/filter/Filter.jsx
+++ client/src/components/filter/Filter.jsx
@@ -1,11 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './filter.scss';
+import { useSearchParams } from 'react-router-dom';
+
+// *** Autre façon de faire un filtre! (FILTRE DYNAMIQUE qui doit survivre à un refresh)
 
 function Filter() {
+  const [searchParams, setSearchParams] = useSearchParams(); // récupère le query dans le req (bare url)
+  const [query, setQuery] = useState({
+    transaction: searchParams.get('transaction') || '',
+    city: searchParams.get('city') || '',
+    property: searchParams.get('property') || '',
+    minPrice: searchParams.get('minPrice') || '',
+    maxPrice: searchParams.get('maxPrice') || '',
+    bedroom: searchParams.get('bedroom') || '',
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target; // contient la valeur tapé par l'utilisateur
+    setQuery((prevQuery) => ({
+      ...prevQuery,
+      [name]: value,
+    }));
+  };
+
+  const handleFilter = () => {
+    setSearchParams(query); // transfome l'objet query en URL (met directement dans l'url sans navigate, form)
+  };
+
   return (
     <section className="filter">
       <h1>
-        Search results for <b>London</b>
+        Search results for <b>{query.city}</b>
       </h1>
       <div className="top">
         <div className="item">
@@ -15,13 +40,20 @@ function Filter() {
             name="city"
             id="city-id"
             placeholder="City location"
+            onChange={handleChange}
+            value={query.city}
           />
         </div>
       </div>
       <div className="bottom">
         <div className="item">
           <label htmlFor="type-id">Type</label>
-          <select name="type" id="type-id">
+          <select
+            name="type"
+            id="type-id"
+            onChange={handleChange}
+            value={query.transaction}
+          >
             <option value="any">any</option>
             <option value="buy">Buy</option>
             <option value="rent">Rent</option>
@@ -29,7 +61,12 @@ function Filter() {
         </div>
         <div className="item">
           <label htmlFor="apartment-id">Apartment</label>
-          <select name="apartment" id="apartment-id">
+          <select
+            name="apartment"
+            id="apartment-id"
+            onChange={handleChange}
+            value={query.property}
+          >
             <option value="any">any</option>
             <option value="house">house</option>
             <option value="condo">condo</option>
@@ -43,6 +80,8 @@ function Filter() {
             name="minPrice"
             id="minPrice-id"
             placeholder="any"
+            onChange={handleChange}
+            value={query.minPrice}
           />
         </div>
         <div className="item">
@@ -52,6 +91,8 @@ function Filter() {
             name="maxPrice"
             id="maxPrice-id"
             placeholder="any"
+            onChange={handleChange}
+            value={query.maxPrice}
           />
         </div>
         <div className="item">
@@ -61,9 +102,11 @@ function Filter() {
             name="bedroom"
             id="bedroom-id"
             placeholder="any"
+            onChange={handleChange}
+            value={query.bedroom}
           />
         </div>
-        <button>
+        <button onClick={handleFilter}>
           <img src="/search.png" alt="Search" />
         </button>
       </div>
